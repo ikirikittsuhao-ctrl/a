3326ae2546f0f250b420a0b2b706ba9ad74f5893
ikirikittsuhao-ctrl <ikirikittsuhao@gmail.com>
2026-08-29 16:37:56 +0900

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.SCHAT_SUPABASE_URL="";
-window.SCHAT_SUPABASE_ANON_KEY="";
+window.SCHAT_SUPABASE_URL="https://wnrzdqskqgepiwawfnbr.supabase.co";
+window.SCHAT_SUPABASE_ANON_KEY="sb_publishable_4MZFy4OOkzUK125XcB1qRg_WrSuVxIR";
